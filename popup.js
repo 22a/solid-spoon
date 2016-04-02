f504a35d11524e7ffdec6188d2d59de4e329b1c5
22a <meehanp2@tcd.ie>
2016-04-02 14:00:10 +0100

    :older_woman added times and welcome messages :older_woman:

--- popup.js
+++ popup.js
@@ -1,3 +1,13 @@
+function addMessage(html){
+  var messageContainer = document.getElementById('messages');
+  messageContainer.innerHTML += html;
+  messageContainer.scrollTop = messageContainer.scrollHeight;
+}
+
+function messageString(payload){
+  return '<p>[' + moment().format('HH:mm') + ']<strong class="message-username">' + payload.username + '</strong>' + payload.message + '</p>';
+}
+
 document.addEventListener('DOMContentLoaded', function() {
   var socket = new Phoenix.Socket("ws://backspoon.cbrenn.xyz/socket", {});
   socket.connect();
@@ -6,18 +16,17 @@ document.addEventListener('DOMContentLoaded', function() {
     dummy.href = tab.url;
     var host = dummy.hostname;
 
-    var messageContainer = document.getElementById('messages');
     var channel = socket.channel("chat:" + host);
 
     channel.on("new_message", function(payload) {
-      messageContainer.innerHTML += '<p><strong class="message-username">' + payload.username + '</strong>' + payload.message + '</p>';
-      messageContainer.scrollTop = messageContainer.scrollHeight;
+      addMessage(messageString(payload));
     });
 
     var userInput = document.getElementById('username');
     userInput.addEventListener('keypress', function(e) {
       if (e.keyCode === 13) {
         document.getElementById('tint').className += " evanesco";
+        document.getElementById('input-container').style.zIndex = "1";
         channel.join().receive("ok", onJoined(channel, host, userInput.value));
       }
     });
@@ -26,8 +35,14 @@ document.addEventListener('DOMContentLoaded', function() {
 
 function onJoined(channel, host, username) {
   return function() {
-    setInterval(function() {
-      channel.push("new_message", {host: host, username: username, message: "POW"});
-    }, 1000);
+    addMessage('<h3 class="centered">Welcome to ' + host + ' chat!</h3>');
+    var messageInput = document.getElementById('message');
+    messageInput.addEventListener('keypress', function(e) {
+      var input = messageInput.value;
+      if (e.keyCode === 13 && input && input.length > 0) {
+        channel.push("new_message", {host: host, username: username, message: messageInput.value})
+        messageInput.value = '';
+      }
+    });
   };
 }
