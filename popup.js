c32a63407a1624428cd90ae09d17885af588b7aa
Eoin Houlihan <ehoulih@tcd.ie>
2016-04-02 11:59:24 +0100

    More spoons

--- popup.js
+++ popup.js
@@ -1,1 +1,14 @@
-console.log("hi");
+function insertBlah(blah) {
+  document.getElementById('blah').textContent += blah;
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  var socket = new Phoenix.Socket("ws://backspoon.cbrenn.xyz", {});
+  socket.connect();
+  chrome.tabs.getSelected(null, function(tab) {
+    var dummy = document.createElement('a');
+    dummy.href = tab.url;
+    var channel = socket.channel("chat:" + dummy.hostname);
+    insertBlah(dummy.hostname);
+  });
+});
