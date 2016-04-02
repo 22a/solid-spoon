bea8f8638301a2a8e7a41a88fbe85fcaa87b46b2
Eoin Houlihan <ehoulih@tcd.ie>
2016-04-02 13:19:53 +0100

    :older_woman: refactor

--- popup.js
+++ popup.js
@@ -7,25 +7,25 @@ document.addEventListener('DOMContentLoaded', function() {
     var host = dummy.hostname;
 
     var channel = socket.channel("chat:" + host);
+
     channel.on("new_message", function(payload) {
       console.log("[" + payload.username + "] " + payload.message);
     });
 
-    var connected = false;
-    var username = "";
     var userInput = document.getElementById('username');
     userInput.addEventListener('keypress', function(e) {
       if (e.keyCode === 13) {
-        username = userInput.value;
-        channel.join();
-        connected = true;
+        channel.join().receive("ok", onJoined(channel, host, userInput.value));
       }
     });
-
-    setInterval(function() {
-      if (connected) {
-        channel.push("new_message", {host: host, username: username, message: "POW"});
-      }
-    }, 1000);
   });
 });
+
+function onJoined(channel, host, username) {
+
+  return function() {
+    setInterval(function() {
+      channel.push("new_message", {host: host, username: username, message: "POW"});
+    }, 1000);
+  };
+}
